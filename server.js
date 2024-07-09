2732aea10d55d0d2cf0bfaa79fde53cb9f966035
maneeshk-chandran <maneesh.maitexa@gmail.com>
2024-07-09 12:12:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const mongoose = require('mongoose');
 const adminRouter = require('./src/routes/adminRouter');
