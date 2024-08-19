82c687fbbad69753c0e40a15d40078c736430341
maneeshk-chandran <maneesh.maitexa@gmail.com>
2024-08-19 16:28:10 +0530

    Update userSchema.js

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
-  login_id: { type: mongoose.Types.ObjectId, ref: 'login_tb' },
+  // login_id: { type: mongoose.Types.ObjectId, ref: 'login_tb' },
   name: { type: String, require: true },
   mobile: { type: String, require: true },
   address: { type: String, require: true },
 });
 
 var userData = mongoose.model('user_tb', userSchema);
+
 module.exports = userData;
